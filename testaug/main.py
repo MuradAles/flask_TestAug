5f852ba429272dde9ca98ecd7983921aa4c97f17
new name <muradaleskerov99@gmail.com.com>
2022-12-19 16:09:33 -0500

    new design

--- testaug/main.py
+++ testaug/main.py
@@ -7,16 +7,41 @@ main = Blueprint('main', __name__)
 # we collect gpt-3 responses here
 arr = []
 
+# A negative sentiment sentence with negative sentiment question and word yes as the answer.
+# A positive sentiment sentence with positive sentiment question and word yes as the answer.
+# My opinion is more important than others’ when expressing positive sentiment.
+
+
 @main.route('/', methods=["POST", "GET"])
 def index():
     request_method = request.method
+    error = []
 
     if request.method == 'POST':
         if "statement" in request.form:
             temp = []
             value = request.form['statement']
+            value = value.split('\n')
+            print(value)
             temp.append(value)
-            out = test.generate_gpt3_test_suite(temp)
+            # if len(value) < 10:
+            #     error.append("The sentence must be at least 10 characters")
+            #     return render_template('index.html', error=error)
+            out = "hell"
+            while len(out) != 5:
+                out = test.generate_gpt3_test_suite(temp)
+
+            for i in range(0, 5):
+                out[i] = out[i].replace('["', ' ').replace(
+                    "['", ' ').replace("']", ' ').replace(
+                    '"]', ' ').replace('[', ' ').replace(
+                    ']', ' ').replace(" , ", ' ').replace(
+                    "', '", ' ').replace(",", ' ').replace(
+                    ", ", ' ').replace('" "', ' ').replace(
+                    "' '", ' ').replace('"', '').replace("'", '')
+            print(out)
+            value = ''.join(value)
+
         else:
             value = 0
         if "output" in request.form:
@@ -24,9 +49,6 @@ def index():
             arr.append(user_like)
         else:
             user_like = 0
-        
-        print(arr)
-
         if value == 0:
             return render_template('index.html', request_method=request_method, user_like=user_like)
         if user_like == 0:
--- testaug/test.py
+++ testaug/test.py
@@ -2,30 +2,31 @@ import os
 import openai
 
 from tqdm import trange
-#from testaug.testaug import query_gpt
 from testaug.testaug.testaug import query_gpt
 
+
 def generate_gpt3_test_suite(
-        texts,
-        query_budget=5,
-        n_per_query=1,
-    ):
+    texts,
+    query_budget=5,
+    n_per_query=1,
+):
     def parse(output):
         texts = list()
         for text in output.split("}"):
             texts.append(text.replace("}", "").replace("{", ""))
         return [text for text in texts if text]
 
-    #openai.api_key = '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'
-    #openai.api_key =  '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'
-    openai.api_key =   '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'
-
+    # openai.api_key = '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'
+    # openai.api_key = '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'
+    # openai.api_key = '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'
+    openai.api_key = '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'
     prompt = "\n".join(["- {{{}}}\n".format(text) for text in texts])
     prompt += "\n- {"
 
     extracted_texts = list()
     for _ in trange(query_budget):
-        response = query_gpt(model="text-davinci-003", prompt=prompt, n_per_query=n_per_query)
+        response = query_gpt(model="text-davinci-003",
+                             prompt=prompt, n_per_query=n_per_query)
 
         for choice in response.choices:
             extracted_texts.extend(parse(choice.text))
@@ -33,12 +34,12 @@ def generate_gpt3_test_suite(
     return extracted_texts
 
 
-texts = [
-    "i love pepperoni pizza",
-    "i like udon noodles",
-    "my favorite food is steak"
-]
+# texts = [
+#     "i love pepperoni pizza",
+#     "i like udon noodles",
+#     "my favorite food is steak"
+# ]
 
-output = generate_gpt3_test_suite(texts)
+# output = generate_gpt3_test_suite(texts)
 
-print(output)
+# print(output)
